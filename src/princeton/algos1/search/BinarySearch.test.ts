312f1ac4189cc1447e634bad24a875a86f5f25fa
Tony Childs <tony.childs@gmail.com>
2023-12-05 23:02:29 -0600

    fix binary search bug and add tests/benchmarks

--- src/princeton/algos1/search/BinarySearch.test.ts
+++ src/princeton/algos1/search/BinarySearch.test.ts
@@ -1,9 +1,66 @@
 import { binarySearch } from "./BinarySearch";
 
-const ints = [6, 33, 43, 51, 53, 13, 14, 93, 95, 96, 97, 25, 64, 72, 84];
-ints.sort((a, b) => a - b);
+function getRandomInt(min: number, max: number) {
+  return Math.round(Math.random() * (max - min) + min);
+}
 
-console.log(ints);
-console.log("binarySearch(ints, 33) = ", binarySearch(ints, 33));
-console.log("binarySearch(ints, 95) = ", binarySearch(ints, 95));
-console.log("binarySearch(ints, 49) = ", binarySearch(ints, 49));
+function getRandomArray(qty: number) {
+  const nums = new Array(qty);
+  for (let i = 0; i < qty; i++) {
+    nums[i] = getRandomInt(0, qty);
+  }
+  return nums;
+}
+
+function generateStats(successes: number[], fails: number[]) {
+  const sum = (sum: number, val: number) => sum + val;
+
+  const successSum = successes.reduce(sum, 0);
+  if (successes.length > 0) {
+    console.log(`Avg success time: ${successSum / successes.length}`);
+  } else {
+    console.log("No successes");
+  }
+
+  const failSum = fails.reduce(sum, 0);
+  if (fails.length > 0) {
+    console.log(`Avg fail time: ${failSum / fails.length}`);
+  } else {
+    console.log("No fails");
+  }
+
+  console.log(
+    `Avg total search time ${
+      (successSum + failSum) / (successes.length + fails.length)
+    }`
+  );
+}
+
+function main() {
+  const nums = getRandomArray(10000);
+  nums.sort((a, b) => a - b);
+  console.log(nums);
+
+  const successes = [];
+  const fails = [];
+
+  for (let i = 0; i < 1000; i++) {
+    const key = getRandomInt(0, 10000);
+    // console.log(`search for ${key}`);
+    const startTime = performance.now();
+    const result = binarySearch(nums, key);
+    const elapsedTime = performance.now() - startTime;
+
+    if (result === -1) {
+      fails.push(elapsedTime);
+      // console.log(`${key} not found`);
+    } else {
+      successes.push(elapsedTime);
+      // console.log(`${key} found at ${result}`);
+    }
+  }
+
+  generateStats(successes, fails);
+}
+
+main();
